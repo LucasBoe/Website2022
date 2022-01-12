09e1466080f7338b69c8bfa24d64ccd9a5bdf544
LucasBoe <lucas@lumotex.de>
2022-01-12 19:34:53 +0100

    improvements to dynamic content loading and added scrolling

--- js/projectTransition.js
+++ js/projectTransition.js
@@ -6,36 +6,40 @@ $(".project-preview").click(function(){
 var sWidth = 0;
 var sHeight = 0;
 var sPos = {left: 0, top: 0};
-var duration = 2000;
+var duration = 500;
 
 openProject = function(from, id) {
 
     var pWindow = $("#project-window");
     var blend = $("#project-popup-blend");
+    var preview = $(pWindow).find(".preview");
 
     pWindow.show();
     blend.show();
 
     $("#project-holder").load('atlb.html');
-    $(from).find(".preview").children(0).clone().appendTo($(pWindow).find(".preview"));
+    var clone = $(from).find(".preview").children(0).clone();
+    clone.appendTo(preview);
+    clone.css("width", "100%")
     $(from).find(".title").clone().appendTo($(pWindow).find(".logo"));
 
     sPos = $(from).position();
-    sWidth = $(from).width();
-    sHeight = $(from).height();
+    sWidth = $(from).width() * 1.2;
+    sHeight = $(from).height() * 1.2;
 
-    $(pWindow).css({width: sWidth, height: sHeight ,top: sPos.top, left: sPos.left});
+    
+    $(pWindow).css({width: sWidth,top: sPos.top, left: sPos.left});
 
-    $(blend).animate({
-        opacity: 0.5
-    }, duration);
+    preview.css("min-width", "0%");
+    $(pWindow).find(".header").css("height", "40vh");
+
+    blend.css("background-color", "rgba(0,0,0,0.5);")
 
     $(pWindow).animate({
         opacity: 1,
         left: "10vw",
         top: "10vh",
-        width: "80vw",
-        height: "100%",
+        width: "80vw"
       }, duration, function() {
         $(pWindow).find("#close").click(function(){
             closeProject();
@@ -50,23 +54,21 @@ closeProject = function() {
     var pWindow = $("#project-window");
     var blend = $("#project-popup-blend");
     $(pWindow).find("#close").off("click");
+    $(pWindow).find(".preview").css("min-width", "100%");
+    $(pWindow).find(".header").css("height", "60vh");
 
-    $(blend).animate({
-        opacity: 0
-    }, duration, function() {
-        $(this).hide();
-    });
+    blend.css("background-color", "rgba(0,0,0,0);")
 
     $(pWindow).animate({
-        opacity: 0.9,
-        left: sPos.left,
-        top: sPos.top,
-        width: sWidth,
-        height: sHeight,
+        opacity: 0.1,
+        left: sPos.left + sWidth * 0.1,
+        top: sPos.top + sHeight * 0.1,
+        width: sWidth / 1.2,
       }, duration, function() {
         $(this).hide();
         $(pWindow).find(".preview").empty();
         $(pWindow).find(".logo").empty();
+        blend.hide();
         setBackgroundBlurred(false);
       })
 };
